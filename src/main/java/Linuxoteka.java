fd9c81d9ab3c426e9f6b9a0fec1b41885367a52d
Marlena Kurczab <mojca13@gazeta.pl>
2025-07-30 21:59:00 +0200

    mini zmiana.

--- src/main/java/Linuxoteka.java
+++ src/main/java/Linuxoteka.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Linuxoteka {
+    public static void main(String[] args) {
+        try{
+            Scanner input = new Scanner(new File("C:/Users/mojca/Desktop/LinuxotekaJava/src/main/java/UbuntuLista.txt"));
+            System.out.print("zawartość pliku: ");
+            while(input.hasNextLine()){
+                System.out.println(input.nextLine());
+            }
+            input.close();
+        }catch (FileNotFoundException e) {
+            System.out.println("Brak pliku.");
+            e.printStackTrace();
+        }
+    }
 }
